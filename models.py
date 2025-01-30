27fc88a68978a31f64000c018ab7b42ce47bad03
Vladdeek <vladryazanov2709@gmail.com>
2025-01-30 23:41:54 +0300

    5.2 Работа с базой данных (Выполнено)

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey #ForeignKey будет ссылаться на поле из другой таблицы 
+from sqlalchemy.orm import relationship # для создания связи между полями 
+from database import Base # все наше подключение которое которое на основе наших моделей создает таблицы в БД
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)# index=True - поиск по этому столбцу
+    name = Column(String, index=True)
+    age = Column(Integer)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    body = Column(String)
+    author_id = Column(Integer, ForeignKey("users.id"))# создаёт связь с таблицей users по столбцу id
+
+    author = relationship("User")# Доп. поле в которое мы будем помещать информацию про пользователя
+    # для этого в схемах в классе Post добавляем author: User
+
+
+# Этот файл описывает каждую табличку для БД
+# на основе этого файла на основе этих классов 
+# будут созданы разные таблицы в БД
---
+++ schemas.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+
+
+
+#User 
+class UserBase(BaseModel):
+    name: str
+    age: int
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+    class Config:
+        orm_mode = True
+    
+
+#Post
+class PostBase(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int 
+    author: User
+    class Config:
+        orm_mode = True
+
+
+
+# Здесь описаны различные схемы,
+# нужны для описания API
+# нужны для описания того что мы будем принимать
+# и что мы будем принимать 
